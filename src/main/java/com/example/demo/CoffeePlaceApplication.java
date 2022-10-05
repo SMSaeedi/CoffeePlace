29cf78882b92c286cca1e12763ee70b4df862edf
SMSaeedi <mahsa.saeedy@gmail.com>
2022-10-05 23:02:06 +0330

    done with mostUsed toppings API changes model properties

--- src/main/java/com/example/demo/CoffeePlaceApplication.java
+++ src/main/java/com/example/demo/CoffeePlaceApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource({"classpath:application.yml"})
 public class CoffeePlaceApplication {
 
 	public static void main(String[] args) {
