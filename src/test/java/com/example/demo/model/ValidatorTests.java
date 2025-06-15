ad22b3c81c6e62f960f91493cb76ffd04892be1e
SMSaeedi <mahsa.saeedy@gmail.com>
2025-06-15 15:44:33 +0200

    compilation fix on test class

--- src/test/java/com/example/demo/model/ValidatorTests.java
+++ src/test/java/com/example/demo/model/ValidatorTests.java
@@ -1,13 +1,13 @@
 package com.example.demo.model;
 
 import com.example.demo.dto.CustomerDto;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.Validator;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
